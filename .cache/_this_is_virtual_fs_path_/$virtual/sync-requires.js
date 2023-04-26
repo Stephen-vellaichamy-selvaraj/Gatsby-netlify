fe4350b87dcd2df9ff1ac4d27b7e80cec20f4b53
Stephen VellaiChamy Selvaraj <stephen.vellaichamyselvaraj@mindtree.com>
2023-04-26 16:08:33 +0100

    Full build update

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("C:\\Development\\Contentful\\ABCDemo\\Gatsby-netlify\\.cache\\dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("C:\\Development\\Contentful\\ABCDemo\\Gatsby-netlify\\src\\pages\\404.js")),
   "component---src-pages-about-js": preferDefault(require("C:\\Development\\Contentful\\ABCDemo\\Gatsby-netlify\\src\\pages\\about.js")),
   "component---src-pages-index-js": preferDefault(require("C:\\Development\\Contentful\\ABCDemo\\Gatsby-netlify\\src\\pages\\index.js")),
--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -1,15 +1,8 @@
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=default" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-about-js": () => import("./../../../src/pages/about.js?export=default" /* webpackChunkName: "component---src-pages-about-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=default" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-projects-index-js": () => import("./../../../src/pages/projects/index.js?export=default" /* webpackChunkName: "component---src-pages-projects-index-js" */),
-  "component---src-templates-project-details-js": () => import("./../../../src/templates/project-details.js?export=default" /* webpackChunkName: "component---src-templates-project-details-js" */)
-}
-
-
-
-exports.head = {
-
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-js": () => import("./../../../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-projects-index-js": () => import("./../../../src/pages/projects/index.js" /* webpackChunkName: "component---src-pages-projects-index-js" */),
+  "component---src-templates-project-details-js": () => import("./../../../src/templates/project-details.js" /* webpackChunkName: "component---src-templates-project-details-js" */)
 }
 
